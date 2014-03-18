f3e0dad610bd04a81cbc4fa1ee1b2f21ad603c89
Georg Konwisser <gekonwi@gmail.com>
2014-03-18 18:08:01 -0400

    added working items/search route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -61,6 +61,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def search
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_item
@@ -71,4 +75,4 @@ class ItemsController < ApplicationController
     def item_params
       params.require(:item).permit(:title, :description, :type)
     end
-end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Lostandfound::Application.routes.draw do
-  resources :items
-
-get "items/search"
+  resources :items do
+   collection do
+    get 'search'
+  end
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
