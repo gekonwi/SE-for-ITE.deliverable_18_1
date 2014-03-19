70b6e2d0c9fa7cf8cfdde9085d7cf6ce581af426
Georg Konwisser <gekonwi@gmail.com>
2014-03-19 12:07:48 -0400

    added owner column, improved seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,65 +22,77 @@ items["Food"] = %w{beef pork chicken bacon ham lamb liver kidneys turkey duck sa
 	rhubarb cod haddock plaice tuna salmon trout mackerel herring sardine pilchard sole anchovy toast jam marmalade breakfast cereal
 	cornflakes muesli porridge}
 
-	positive_adjectives = %w{amazing awesome blithesome excellent fabulous fantastic favorable fortuitous great
-		incredible ineffable mirthful outstanding perfect propitious remarkable smart spectacular splendid stellar
-		stupendous super ultimate unbelievable wondrous}
+	positive_adjectives = ['good', 'super', 'fine', 'usable', 'pretty', 'probably usable' ]
+	negative_adjectives = ['damaged', 'squeezed', 'dirty', 'scratched', 'water damaged', 'bended', 'broken' ]
 
-		taste_adjectives = %w{alkaline bitter bittersweet bland burnt buttery crisp fishy fruity gingery hearty hot mellow oily
-			overripe peppery raw ripe salty sour spicy spoiled sugary sweet tangy medicinal tasteless unripe vinegary }
+	taste_adjectives = %w{alkaline bitter bittersweet bland burnt buttery crisp fishy fruity gingery hearty hot mellow oily
+		overripe peppery raw ripe salty sour spicy spoiled sugary sweet tangy medicinal tasteless unripe vinegary }
 
-			colors = %w{amber amethyst aqua aquamarine avocado azure bistre black blue brass bright brilliant brindle bronze
-				buff burgundy canary carmine carnelian cerise charcoal chartreuse chestnut chocolate chrome citrine claret clear
-				cobalt copper coral cordovan cream crimson crystalline cyan dark drab dull dun ebony emerald flesh flushed fuchsia
-				garnet glassy gold green grizzly henna indigo iridescent ivory jade jet khaki lake lavender lemon light lilac lime
-				magenta mahogany maize maroon mauve milky mint mustard navy obsidian ocher olive onyx opaque orange orchid
-				pale peach pearl pearly pink plum poppy primrose puce purple red rose ruby ruddy rust sable saffron salmon sapphire
-				scarlet sepia shimmering sienna silver slate smoky snowy sooty spruce tan topaz translucent transparent turquoise
-				twinkling ultramarine umber vermilion violet walnut white wine yellow}
+		colors = %w{amber amethyst aqua aquamarine avocado azure bistre black blue brass bright brilliant brindle bronze
+			buff burgundy canary carmine carnelian cerise charcoal chartreuse chestnut chocolate chrome citrine claret clear
+			cobalt copper coral cordovan cream crimson crystalline cyan dark drab dull dun ebony emerald flesh flushed fuchsia
+			garnet glassy gold green grizzly henna indigo iridescent ivory jade jet khaki lake lavender lemon light lilac lime
+			magenta mahogany maize maroon mauve milky mint mustard navy obsidian ocher olive onyx opaque orange orchid
+			pale peach pearl pearly pink plum poppy primrose puce purple red rose ruby ruddy rust sable saffron salmon sapphire
+			scarlet sepia shimmering sienna silver slate smoky snowy sooty spruce tan topaz translucent transparent turquoise
+			twinkling ultramarine umber vermilion violet walnut white wine yellow}
 
-				times = ["yestarday", "last week", "on 03/12", "today morning", "on Monday", "on Tuesday", "on Wednesday", "some days ago", "tonight"]
+			times = ["yestarday", "last week", "on 03/12", "today morning", "on Monday", "on Tuesday", "on Wednesday", "some days ago", "tonight"]
 
-				people = ["some employee", "a student", "John", "Prof. McGonagall", "Harry", "someone"]
+			activities = ["on the way home", "while walking to the parking slot", "leaving campus", "walking near the parc", ""]
 
-				activities = ["on the way home", "while walking to the parking slot", "leaving campus", "walking near the parc", ""]
+			places = ["in the toilet", "near the gym", "in room 774", "in the CS office", "on the street", "behind a tree", "next to a car"]
 
-				places = ["in the toilet", "near the gym", "in room 774", "in the CS office", "on the street", "behind a tree", "next to a car"]
+			bad_quality_intros = ["a little", "seems to be", "might be", "although a little"]
+			good_quality_intros = ["condition:", "looks", "still looks", "should still be"]
 
-				quality_intros = ["condition:", "looks", "still looks", "a little bit damaged, but other than that it's", "assumed quality:"]
+			taste_intros = ["it still tastes", "seems to be", "tastes", "it tastes"]
 
-				taste_intros = ["it still tastes", "seems to be", "tastes", "it tastes"]
+			names = ['Melynda Corder', 'Sylvie Verges', 'Neely Rasberry', 'Octavia Hibbert', 'Shawana Leggett', 'Nell Lesh', 'Lindsay Haefner',
+				'Charisse Holtzclaw', 'Cole Kula', 'Alina Spires', 'Margret Perri', 'Annabelle Seel', 'Gussie Fretwell', 'Inell Burgio', 'Nydia Mcgivney',
+				'Dominic Arner', 'Madlyn Casebeer', 'Dayle Walpole', 'Inocencia Howells', 'Vallie Sue', 'Anya Mccutcheon', 'Jazmin Coty',
+				'Debroah Seagle', 'Marilyn Mireles', 'Vince Mcgowen']
 
 				100.times do
 					type = Type.all.sample
 
-					title = ""
-					title += positive_adjectives.sample + " " if rand(3) == 0
-					title += items[type.title].sample
-					title += " (#{colors.sample})" if type.title != "Food" && rand(3) == 0
+					title = items[type.title].sample
+					title += " (#{colors.sample})" if ["Clothes", "Furniture"].include?(type.title) && rand(2) == 0
 
 					case rand(4)
 					when 0
-						description = "found by #{people.sample}"
+						description = "found by #{names.sample}"
 						description += " #{times.sample}" if rand(2) == 0
 					when 1
-						description = "#{times.sample} #{people.sample} found it #{places.sample}"
+						description = "#{times.sample} #{names.sample} found it #{places.sample}"
 						description += " #{activities.sample}" if rand(2) == 0
 					when 2
-						description = "#{people.sample} brought it #{times.sample}"
+						description = "#{names.sample} brought it #{times.sample}"
 					when 3
 						description = "was found #{places.sample}"
 						description += " #{times.sample}" if rand(2) == 0
 					end
 
-					description += "; #{quality_intros.sample} #{positive_adjectives.sample}"
+					case rand(4)
+					when 0
+
+					when 1
+						description += "; #{bad_quality_intros.sample} #{negative_adjectives.sample}"
+					when 2
+						description += "; #{good_quality_intros.sample} #{positive_adjectives.sample}"
+					when 3
+						description += "; #{bad_quality_intros.sample} #{negative_adjectives.sample},
+						#{good_quality_intros.sample} #{positive_adjectives.sample}"
+					end
 
 					if rand(2) == 0 && type.title == "Food"
 						description += "; "
-						description += "#{people.sample} tried it, " if rand(2) == 0
+						description += "#{names.sample} tried it, " if rand(2) == 0
 						description += "#{taste_intros.sample} #{taste_adjectives.sample}"
 					end
 
 					item = Item.new(title: title, description: description)
 					item.type = type
+					item.owner = names.sample if rand(2) == 0
 					item.save
 				end
