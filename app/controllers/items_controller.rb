65488230c553001c0f67761ca785313304cbb538
Georg Konwisser <gekonwi@gmail.com>
2014-03-20 03:34:48 -0400

    restricted routes deleted al files which should not be uses such as the ones for editing, adding, and showing single items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,60 +7,6 @@ class ItemsController < ApplicationController
 		@items = Item.all
 	end
 
-	# GET /items/1
-	# GET /items/1.json
-	def show
-	end
-
-	# GET /items/new
-	def new
-		@item = Item.new
-	end
-
-	# GET /items/1/edit
-	def edit
-	end
-
-	# POST /items
-	# POST /items.json
-	def create
-		@item = Item.new(item_params)
-
-		respond_to do |format|
-			if @item.save
-				format.html { redirect_to @item, notice: 'Item was successfully created.' }
-				format.json { render action: 'show', status: :created, location: @item }
-			else
-				format.html { render action: 'new' }
-				format.json { render json: @item.errors, status: :unprocessable_entity }
-			end
-		end
-	end
-
-	# PATCH/PUT /items/1
-	# PATCH/PUT /items/1.json
-	def update
-		respond_to do |format|
-			if @item.update(item_params)
-				format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-				format.json { head :no_content }
-			else
-				format.html { render action: 'edit' }
-				format.json { render json: @item.errors, status: :unprocessable_entity }
-			end
-		end
-	end
-
-	# DELETE /items/1
-	# DELETE /items/1.json
-	def destroy
-		@item.destroy
-		respond_to do |format|
-			format.html { redirect_to items_url }
-			format.json { head :no_content }
-		end
-	end
-
 	def search
 		# the default search mode is OR
 		@sel_mode = params[:mode] || 'or'
@@ -103,14 +49,4 @@ class ItemsController < ApplicationController
 
 		items
 	end
-
-		# Use callbacks to share common setup or constraints between actions.
-		def set_item
-			@item = Item.find(params[:id])
-		end
-
-		# Never trust parameters from the scary internet, only allow the white list through.
-		def item_params
-			params.require(:item).permit(:title, :description, :type)
-		end
-	end
+end
