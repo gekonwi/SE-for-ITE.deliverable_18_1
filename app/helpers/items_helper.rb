f9ff4ed4057167cb1a0059b78dc2a5d3b10fbe5b
Georg Konwisser <gekonwi@gmail.com>
2014-03-20 03:24:57 -0400

    fixed header selection: now the active page is represented with a corresponding selection in the navigation bar

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+
+	# returns 'class=active' if cur_page == page, otherwise an empty string
+	def active_if(cur_page, page)
+		return 'class=active' if cur_page == page
+		return ""
+	end
+
 end
